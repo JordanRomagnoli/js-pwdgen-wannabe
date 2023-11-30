10385e174342edf468dc31aa1f80b55403479446
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-11-30 13:56:25 +0100

    - SET variables - ADD prompt function - ADD console log - ADD innerHTML

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const userName = prompt("Qual è il tuo nome ?");
+const userSurname = prompt("Qual è il tuo cognome ?");
+const userFavoriteColor = prompt("Qual è il tuo colore preferito ?");
+let classNumber = 114;
+
+
+console.log(
+    ` Il nome dell'utente è : ${userName}`
+);
+
+console.log(
+    ` Il cognome dell'utente è : ${userSurname}`
+);
+
+console.log(
+    ` Il colore preferito dell'utente è : ${userFavoriteColor}`
+);
+
+document.getElementById('username').innerHTML = `${userName}${userSurname}${userFavoriteColor}${classNumber}`;
